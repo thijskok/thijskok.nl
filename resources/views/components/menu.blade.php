a46e3ccbbac32c439279493d7a11f26ca5848021
Thijs Kok <mail@thijskok.nl>
2018-09-07 09:04:33 +0200

    Responsive menu fix

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -1,53 +1,60 @@
 <nav class="navbar" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
         <a href="{{ route('home') }}" class="navbar-item">
-            {{ config('app.name') }}
+            <p>{{ config('app.name') }}</p>
         </a>
 
-        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+        <a role="button" class="navbar-burger" data-target="navMenu" aria-label="menu" aria-expanded="false">
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
         </a>
     </div>
 
-    <div class="navbar-end">
-        <a href="https://github.com/thijskok" class="navbar-item">
-            <span class="icon">
-                <i class="fab fa-github"></i>
-            </span>
-        </a>
-        <a href="https://twitter.com/thijskok/" class="navbar-item">
-            <span class="icon">
-                <i class="fab fa-twitter"></i>
-            </span>
-        </a>
-        <a href="/feed" class="navbar-item">
-            <span class="icon">
-                <i class="fas fa-rss"></i>
-            </span>
-        </a>
-
-        @if (Auth::guest())
-            <a class="navbar-item " href="{{ route('login') }}">
+    <div class="navbar-menu" id="navMenu">
+        <div class="navbar-end">
+            <a href="https://github.com/thijskok" class="navbar-item">
                 <span class="icon">
-                    <i class="fas fa-sign-in-alt"></i>
+                    <i class="fab fa-github"></i>
                 </span>
+                <span class="is-hidden-desktop">Github</span>
             </a>
-            {{--<a class="navbar-item " href="{{ route('register') }}">Register</a>--}}
-        @else
-            <a class="navbar-item" href="{{ route('logout') }}"
-               onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+            <a href="https://twitter.com/thijskok/" class="navbar-item">
                 <span class="icon">
-                    <i class="fas fa-sign-out-alt"></i>
+                    <i class="fab fa-twitter"></i>
                 </span>
+                <span class="is-hidden-desktop">Twitter</span>
+            </a>
+            <a href="/feed" class="navbar-item">
+                <span class="icon">
+                    <i class="fas fa-rss"></i>
+                </span>
+                <span class="is-hidden-desktop">RSS</span>
             </a>
 
-            <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                  style="display: none;">
-                {{ csrf_field() }}
-            </form>
-        @endif
+            @if (Auth::guest())
+                <a class="navbar-item " href="{{ route('login') }}">
+                    <span class="icon">
+                        <i class="fas fa-sign-in-alt"></i>
+                    </span>
+                    <span class="is-hidden-desktop">Sign in</span>
+                </a>
+                {{--<a class="navbar-item " href="{{ route('register') }}">Register</a>--}}
+            @else
+                <a class="navbar-item" href="{{ route('logout') }}"
+                   onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                    <span class="icon">
+                        <i class="fas fa-sign-out-alt"></i>
+                    </span>
+                    <span class="is-hidden-desktop">Sign out</span>
+                </a>
 
+                <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                      style="display: none;">
+                    {{ csrf_field() }}
+                </form>
+            @endif
+
+        </div>
     </div>
 </nav>
