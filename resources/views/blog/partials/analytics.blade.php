7627b80e0d2c0c087c788c60baa0a8773fff1612
Thijs Kok <tkok@testmonitor.com>
2020-01-05 22:47:12 +0100

    Added Google Analytics

---
+++ resources/views/blog/partials/analytics.blade.php
@@ -0,0 +1,8 @@
+<script async src="https://www.googletagmanager.com/gtag/js?id={{ env('GOOGLE_ANALYTICS_TRACKING_ID') }}"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', '{{ env('GOOGLE_ANALYTICS_TRACKING_ID') }}');
+</script>
